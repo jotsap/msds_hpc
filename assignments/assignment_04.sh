b83ca020161366213329d2d7fb3a1cc43b6a7812
jotsap <47485481+jotsap@users.noreply.github.com>
2022-05-15 17:07:59 -0700

    Create assignment_04.sh

---
+++ assignments/assignment_04.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# download gullivers travels
+curl https://www.gutenberg.org/files/829/829-0.txt --output travel.txt
+
+# split into 5 equal chunks
+split --number=l/5 travel.txt travelsplit
+
+# call python prime loop
+python prime.py
+
+# move splits into folders
+mv travelsplitaa folder1/
+mv travelsplitab folder2/
+mv travelsplitac folder3/
+mv travelsplitad folder5/
+mv travelsplitae folder7/
